44ab98eed9aea47ad38c76b5dd2e7c0680d16a3e
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-07-12 13:41:05 -0500

    added function

---
+++ script.js
@@ -0,0 +1,68 @@
+const textArea = document.querySelector(".text-area");
+const textAreaMsg = document.querySelector(".text-area-msg");
+
+const btnEncripta = document.querySelector(".btn-encriptar");
+const btnDesencripta = document.querySelector(".btn-desencriptar");
+const btnCopiar = document.querySelector(".copiar");
+
+
+function validarTexto() {
+    let textoEscrito = document.querySelector(".text-area").value;
+    let validador = textoEscrito.match(/^[a-z]*$/);
+
+    if (!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos");
+        location.reload();
+        return true;
+    }
+}
+
+function btnEncriptar() {
+    if (!validarTexto()) {
+        const textoEncriptado = encriptar(textArea.value);
+        textAreaMsg.value = textoEncriptado;
+        textArea.value = "";
+    }
+}
+
+//Laves de encriptacion
+// `La letra "e" es convertida para "enter"`
+// `La letra "i" es convertida para "imes"`
+// `La letra "a" es convertida para "ai"`
+// `La letra "o" es convertida para "ober"`
+// `La letra "u" es convertida para "ufat"`
+
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value);
+    textAreaMsg.value = textoEncriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada;
+}
+
+
+btnEncripta.addEventListener("click", btnEncriptar);
+btnDesencripta.addEventListener("click", btnDesencriptar);
